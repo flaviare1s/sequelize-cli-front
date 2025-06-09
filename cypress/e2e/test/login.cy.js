5a031efd7eae7ef6872e99ba2cf4b2e60949bed6
Flávia Reis <flaviarster@gmail.com>
2025-06-09 11:57:50 -0300

    ajusta teste de login

--- cypress/e2e/test/login.cy.js
+++ cypress/e2e/test/login.cy.js
@@ -1,13 +1,30 @@
-describe("Login correto com usuário verificado!", () => {
-  it("Deve realizar login corretamente, com usuário admin válido.", () => {
-    cy.visit("http://localhost:5173/login");
+describe('Login correto com usuário verificado!', () => {
+  it('Deve realizar login corretamente com usuário admin válido', () => {
+    cy.visit('/login'); 
 
     cy.get('[placeholder="E-mail"]')
-      .type('pokihod589@eduhed.com')
-      .should('have.value', 'pokihod589@eduhed.com');
+      .type('testecypress69@gmail.com')
+      .should('have.value', 'testecypress69@gmail.com');
 
     cy.get('[placeholder="Senha"]')
-    .type('123456')
-    .should('have.value', '123456');
+      .type('Senha123')
+      .should('have.value', 'Senha123');
+
+    cy.intercept('POST', '**/auth/login').as('login');
+
+    cy.wait(100);
+    cy.get('.bg-blue-600').click();
+
+    cy.wait('@login').its('request.body').should((body) => {
+      expect(body.email).to.eq('testecypress69@gmail.com');
+      expect(body.senha).to.eq('Senha123');
+    });
+
+    cy.url().should('not.include', '/login');
+
+    cy.window().should((win) => {
+      const token = win.localStorage.getItem('token');
+      expect(token).to.be.a('string').and.not.be.empty;
+    });
   });
-});
+})
